d6f531df586049eadd5d3d6564c301e12dd3a51d
Duke <dukelin@va7c>
2021-04-06 22:56:35 +0800

    || v1.0.0.02 || Add || 將ToDOList的Data呈現於畫面上  ||

--- VueByAspNet_Mvc/Properties/AssemblyInfo.cs
+++ VueByAspNet_Mvc/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // 您可以指定所有值或預設修訂和組件數目
 // 指定為預設值:
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
